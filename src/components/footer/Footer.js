54864e371447bfa5c9f111ebe65357b321cdd83f
Stanescu Victor <stanescuvictor12@gmail.com>
2023-07-25 21:42:37 +0200

    Footer Adaugat

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,14 +1,38 @@
 import React from "react";
 import "./footer.css";
+import logo from "./img/logo-footer.png";
+import fb from "./img/fb.png";
+import ig from "./img/ig.png";
+import gh from "./img/gh.png";
 
 function Footer() {
   return (
-    <div>
+    <div className="footer">
       <div className="container">
-        <div className="community">
-          <h3>JOIN OUR COMMUNITY</h3>
+        <div className="smaller-container">
+          <div className="community">
+            <h3 id="com">JOIN OUR COMMUNITY</h3>
+            <section className="links">
+              <a href="#">
+                <img src={fb}></img>
+              </a>
+              <a href="#">
+                <img src={ig}></img>
+              </a>
+              <a href="#">
+                <img src={gh}></img>
+              </a>
+            </section>
+          </div>
+          <div className="pages-links">
+            <a href="#">About</a>
+            <a href="#">FAQ</a>
+            <a href="#">Contact Us</a>
+          </div>
         </div>
+        <img className="logo" src={logo}></img>
       </div>
+      <div className="copyright">© 2023 JACK LEMKUS - ALL RIGHTS RESERVED</div>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Carusel />
+      <Footer />
     </div>
   );
 }
