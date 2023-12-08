b447422663ee312ce637754cfb57fb92de5e4f36
Stanescu Victor <stanescuvictor12@gmail.com>
2023-12-08 17:29:19 +0100

    auth firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 // FIREBASE configuration 
 const firebaseConfig = {
-    apiKey:process.env.REACT_APP_APIKEY,
-    authDomain:process.env.REACT_APP_AUTHDOMAIN ,
-    projectId:process.env. REACT_APP_PROJECTID,
-    storageBucket:process.env.REACT_APP_STORAGEBUCKET ,
-    messagingSenderId:process.env.REACT_APP_MESSAGINGSENDERID ,
-    appId:process.env.REACT_APP_APPID ,
-    measurementId: process.env.REACT_APP_MEASURMENTID
+    apiKey:process.env.local.REACT_APP_APIKEY,
+    authDomain:process.env.local.REACT_APP_AUTHDOMAIN ,
+    projectId:process.env.local. REACT_APP_PROJECTID,
+    storageBucket:process.env.local.REACT_APP_STORAGEBUCKET ,
+    messagingSenderId:process.env.local.REACT_APP_MESSAGINGSENDERID ,
+    appId:process.env.local.REACT_APP_APPID ,
+    measurementId: process.env.local.REACT_APP_MEASURMENTID
 };
 
 //starting the app
