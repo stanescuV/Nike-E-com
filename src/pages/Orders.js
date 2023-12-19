d2b49db84b5ae92d6d5cd187c0c57194c553cc62
Stanescu Victor <stanescuvictor12@gmail.com>
2023-12-19 23:22:01 +0100

    working on fetch MyOrders... issues

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -5,33 +5,32 @@ function Orders() {
 
      const {currentUser}= useAuth();
     
-    //date orderID
-    const [date, setDate] = useState([]);
-    //date orderDetails
+    //data orderID
+    const [data, setData] = useState([]);
+    //data orderDetails
     const[ordDetails, setOrdDetails] = useState([]);
-    console.log(currentUser)
     //fetch to get the current user's orders 
     useEffect(()=>{
-        fetch(`http://localhost:3001/orders/O99a89TUPaWFNnvePaFNqckOuN92`)
-        .then(binar => binar.json())
-        .then(json => setDate(json))
-
-    },[])
+        fetch(`http://localhost:3001/orders/${(currentUser && currentUser.uid)}`)
+        .then(r => r.json())
+        .then(rr=> setData(rr))
+       
+     //fetch to get the current user's detailed orders
+        
+       fetch(`http://localhost:3001/orders/${(currentUser && currentUser.uid)}/3`)
+        .then(r => r.json())
+        .then(rr => setOrdDetails(rr))
+        console.log(ordDetails)
+    },[currentUser])
     
-    //fetch to get the current user's detailed orders
-    useEffect(()=>{
-        fetch(`http://localhost:3001/orders/O99a89TUPaWFNnvePaFNqckOuN92/3`)
-        .then(binar => binar.json())
-        .then(json => setOrdDetails(json))
-    },[])
     
-     console.log(date)
 
-     function renderDate (date){
-        return date.map(comanda=> {return (
+     function renderData (data){
+        return data.map(comanda=> {return (
             <div>
                 OrderID : {comanda.orderID}
                 Total : {comanda.total}
+                <br/>
             </div>
         )})}
 
@@ -51,7 +50,7 @@ function Orders() {
 
   return (
     <div style={{display : "flex", justifyContent: "center",flexDirection:"column", alignItems: "center", margin: "200px"}}>
-        {date && renderDate(date)}
+        {data && renderData(data)}
         {ordDetails&& renderOrdDetails(ordDetails)}
         
     </div>
