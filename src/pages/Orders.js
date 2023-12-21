b9b903a57b7579c848bb4facbb2646452ae2463b
Stanescu Victor <stanescuvictor12@gmail.com>
2023-12-21 23:35:01 +0100

    /o4ers looks good

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,60 +1,91 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 
 function Orders() {
+  const { currentUser } = useAuth();
 
-     const {currentUser}= useAuth();
-    
-    //data orderID
-    const [data, setData] = useState([]);
-    //data orderDetails
-    const[ordDetails, setOrdDetails] = useState([]);
-    //fetch to get the current user's orders 
-    useEffect(()=>{
-        fetch(`http://localhost:3001/orders/${(currentUser && currentUser.uid)}`)
-        .then(r => r.json())
-        .then(rr=> setData(rr))
-       
-     //fetch to get the current user's detailed orders
-        
-       fetch(`http://localhost:3001/orders/${(currentUser && currentUser.uid)}/3`)
-        .then(r => r.json())
-        .then(rr => setOrdDetails(rr))
-        console.log(ordDetails)
-    },[currentUser])
-    
-    
+  // Data orderID
+  const [data, setData] = useState([]);
+  // Data orderDetails
+  const [ordDetails, setOrdDetails] = useState([]);
+  // Selected order ID
+  const [selectedOrderID, setSelectedOrderID] = useState(null);
 
-     function renderData (data){
-        return data.map(comanda=> {return (
-            <div>
-                OrderID : {comanda.orderID}
-                Total : {comanda.total}
-                <br/>
-            </div>
-        )})}
+  // State to handle modal visibility
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-        function renderOrdDetails(ordDetails){
-            return ordDetails.map((prod)=>{return (
-                <div>
-                    <p>
-                    Product : {prod.name} 
-                    </p> 
-                    <br/>
-                    <p>Product price: {prod.price}</p>
-                    <br/>
-                    <p>Product quantity bought: {prod.qt}</p>
-                </div>
-            )})
+  // Fetch to get the current user's orders
+  useEffect(() => {
+    fetch(`http://localhost:3001/orders/${currentUser && currentUser.uid}`)
+      .then((r) => r.json())
+      .then((rr) => setData(rr));
+  }, [currentUser]);
+
+  // Function to open/close the modal
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+  
+  
+
+  // Function to fetch order details and open the modal
+  async function handleViewDetails(orderID) {
+    try {
+      if (selectedOrderID !== orderID || !isModalOpen) {
+        const response = await fetch(`http://localhost:3001/orders/${currentUser && currentUser.uid}/${orderID}`);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
         }
+        const orderDetailsData = await response.json();
+        setOrdDetails(orderDetailsData);
+        setSelectedOrderID(orderID);
+        if(!isModalOpen){
+          toggleModal();
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching order details:', error);
+    }
+  }
+
+  function renderData(data) {
+    return data.map((comanda) => {
+      return (
+        <div key={comanda.orderID}>
+          <div>
+            OrderID: {comanda.orderID} Total: {comanda.total}$
+            <button onClick={() => handleViewDetails(comanda.orderID)}>View Details</button>
+          </div>
+          <br />
+        </div>
+      );
+    });
+  }
 
   return (
-    <div style={{display : "flex", justifyContent: "center",flexDirection:"column", alignItems: "center", margin: "200px"}}>
-        {data && renderData(data)}
-        {ordDetails&& renderOrdDetails(ordDetails)}
-        
+    <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', margin: '200px' }}>
+      {data && renderData(data)}
+
+      {/* Render the modal */}
+      {isModalOpen && (
+        <div>
+          {/* Your modal content here */}
+          <div>
+            <h2>Order Details</h2>
+            {ordDetails.map((prod) => (
+              <div key={prod.productId}>
+                <p>Product: {prod.name}</p>
+                <p>Product price: {prod.price}$</p>
+                <p>Product quantity bought: {prod.qt}</p>
+                <hr />
+              </div>
+            ))}
+          </div>
+          <button onClick={toggleModal}>Close Modal</button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Orders
+export default Orders;
